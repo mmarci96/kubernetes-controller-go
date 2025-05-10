b146e24930137cb5974937d2ecc50e4051fd69f1
mmarci96 <sarosdimarci@gmail.com>
2025-05-11 00:55:28 +0200

    renaming kubeclient to watcher

--- proxy/cmd/main.go
+++ proxy/cmd/main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"log"
 
-	kubeclient "github.com/mmarci96/kubernetes-controller-go/proxy/pkg/kube_client"
 	"github.com/mmarci96/kubernetes-controller-go/proxy/pkg/server"
+	"github.com/mmarci96/kubernetes-controller-go/proxy/pkg/watcher"
 )
 
 func main() {
-	go kubeclient.WatchEndpointSlices()
+	go watcher.WatchEndpointSlices()
 	err := server.Run()
 	if err != nil {
 		log.Fatalf("Failed to start server: %v", err)
--- proxy/pkg/watcher/watcher.go
+++ proxy/pkg/watcher/watcher.go
@@ -1,4 +1,4 @@
-package kubeclient
+package watcher
 
 import (
 	"context"
